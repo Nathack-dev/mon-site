a8c4dd7f352263bbf9205d19983a5d7784b005e6
Nathack <nathanmure42@gmail.com>
2022-10-16 20:48:34 +0200

    Add NSFW page

---
+++ nathack.fr/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Nathack</title>
+
+    <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.discordapp.com/attachments/1029897058720698389/1031275193018101870/nathack-modified.png">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap" rel="stylesheet">
+    <link href="css/terminal.css?<?php echo time();?>" rel="stylesheet" />
+  </head>
+  <body>
+    <div id="terminal">
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
+    <script src="js/terminal.js?<?php echo time();?>"></script>
+    <script src="js/defaultcommands.js?<?php echo time();?>"></script>
+  </body>
+</html>
